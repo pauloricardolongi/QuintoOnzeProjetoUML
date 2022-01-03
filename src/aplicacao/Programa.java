ef0b5390aed7cf37298ce33d8bf5b837fb37ed2d
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-03 00:51:25 -0300

    hashCode

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,15 +1,18 @@
 package aplicacao;
 
+import entities.Client;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
-	//equals lento-
-		String a = "Maria";
-		String b = "Alex";
-		System.out.println(b.equals(a));
+	
+		Client c1 = new Client("Maria","maria@gmail.com");
+		Client c2 = new Client("Alex","alex@gmail.com");
 		
-		
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
 	}
 
 }
