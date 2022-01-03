987007f4287e53599dda49cebedad1f9e5ed17f2
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-03 00:20:53 -0300

    equals  lento

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+	//equals lento-
+		String a = "Maria";
+		String b = "Alex";
+		System.out.println(b.equals(a));
+		
+		
+	}
+
+}
