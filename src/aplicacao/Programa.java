7ef36f4566315dd6fb40a62f6d7734aec832b906
pauloricardolongi <pauloricardolongi@gmail.com>
2022-01-03 03:01:45 -0300

    adiciona, remove

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,19 +1,22 @@
 package aplicacao;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-			Set<String> set = new TreeSet<>();
+			Set<String> set = new LinkedHashSet<>();
 			
-			set.add("TV");
+			set.add("Tv");
 			set.add("Notebook");
 			set.add("Tablet");
 			set.add("caneta");
-			System.out.println(set.contains("Notebook"));
+		    
+			//set.remove("Tablet");
+			//set.removeIf(x -> x.length() >=3);
+			set.removeIf(x -> x.charAt(0) == 'T');
 			
 			for (String p : set) {
 			System.out.println(p);
